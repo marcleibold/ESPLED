0398f94352adf52fa248aa5e4744ef25cc39fc6e
Marc Leibold <leibold485@gmail.com>
2021-01-16 14:19:18 +0100

    implemented connection procedure

--- app.ino
+++ app.ino
@@ -3,9 +3,10 @@
 #include <ESP8266WiFi.h>
 
 #define REST_PORT 8080
+#define MAX_RECONNECT 10
 
-String ssid = WiFi.macAddress() + "_network"; // unique network per esp
-String password = "espdefault";
+String defaultSSID = WiFi.macAddress() + "_network"; // unique network per esp
+String defaultPassword = "espdefault";
 
 ESP8266WebServer server(REST_PORT);
 
@@ -13,16 +14,16 @@ void setup()
 {
     Serial.begin(115200);
     WiFi.setSleepMode(WIFI_NONE_SLEEP);
-    WiFi.softAP(ssid, password);
+    WiFi.softAP(defaultSSID, defaultPassword);
     // Serial.print("Connecting");
     // while (WiFi.status() != WL_CONNECTED)
     // {
     //     delay(500);
     //     Serial.print(".");
     // }
-    // Serial.println("");
+    Serial.println("");
     Serial.print("IP: ");
-    Serial.print(WiFi.softAPIP());
+    Serial.println(WiFi.softAPIP());
     routing();
     server.begin();
 }
@@ -37,6 +38,48 @@ void routing()
     server.on("/", HTTP_GET, []() {
         server.send(200, "text/html", "ESP8266 REST Server\n");
     });
+
+    server.on("/connect", HTTP_PUT, []() {
+        if (server.hasArg("plain") == false) {
+            server.send(400);
+            return;
+        }
+
+        StaticJsonDocument<139> doc;
+        String body = server.arg("plain");
+        Serial.println(body);
+        deserializeJson(doc, body);
+        String ssid = doc["ssid"];
+        String pass = doc["pass"];
+        Serial.println(ssid);
+        Serial.println(pass);
+        if (ssid == NULL && pass == NULL) {
+            server.send(400);
+            return;
+        }
+        Serial.println("In Progress");
+        // WiFi.softAPdisconnect(true);
+        WiFi.begin(ssid, pass);
+        uint8 retries = 0;
+        while ((WiFi.status() != WL_CONNECTED) && retries < MAX_RECONNECT) { //go back to AP Mode
+            delay(500);
+            Serial.print(".");
+            retries++;
+        }
+        Serial.println("");
+        if (WiFi.status() != WL_CONNECTED) {
+            Serial.println("Connection failed, reverting");
+            server.send(400);
+            WiFi.disconnect();
+            Serial.println("Back to normal.");
+        } else {
+            Serial.print("Connected to: ");
+            Serial.println(ssid);
+            server.send(201, "text/plain", "Connected to " + ssid + " as " + WiFi.localIP().toString() + "\n");
+            Serial.println("Done.");
+        }
+    });
+
     server.on("/rgb", HTTP_POST, []() {
         if (server.hasArg("plain") == false) {
             server.send(400);
@@ -56,4 +99,10 @@ void routing()
     });
 }
 
-void changeColor(uint8 r, uint8 g, uint8 b) { }
+void changeColor(uint8 r, uint8 g, uint8 b)
+{
+    if (r == 0 && g == 0 && b == 0) {
+        //off
+    } else {
+    }
+}
